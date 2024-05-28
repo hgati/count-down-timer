f63303cd1c84962f1807e54f851233b81c2ea059
its me <hgati@users.noreply.github.com>
2024-05-28 16:06:50 +0900

    Update Timer.php

--- Block/Timer.php
+++ Block/Timer.php
@@ -2,26 +2,29 @@
 namespace Hgati\CountDownTimer\Block;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\ProductFactory;
 
 class Timer extends Template
 {
-    protected $_product;
+    protected $productFactory;
 
     public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
+        Template\Context $context,
+        ProductFactory $productFactory,
         array $data = []
     ) {
-        $this->_product = $context->getProduct();
+        $this->productFactory = $productFactory;
         parent::__construct($context, $data);
     }
 
     public function getCountdownTime()
     {
-        $product = $this->_product;
+        $product = $this->productFactory->create()->load($this->_request->getParam('id'));
         $releaseDate = $product->getData('amasty_preorder_release_date');
-        
         if ($releaseDate) {
+            $releaseDateTimestamp = strtotime($releaseDate);
+            $releaseDateTimestamp = strtotime(date('Y-m-d 23:59:59', $releaseDateTimestamp));
+            $releaseDate = date('Y-m-d H:i:s', $releaseDateTimestamp);
             return $releaseDate;
         } else {
             return null; // Release date not set
